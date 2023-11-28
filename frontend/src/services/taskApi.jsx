3120965357ffd60cc8af283f82f959408b16a0cc
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2023-11-29 00:43:27 +0500

    Service APIs for Task

---
+++ frontend/src/services/taskApi.jsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:3000"; // Replace with your server URL
+
+const api = axios.create({
+  baseURL,
+});
+
+// Create a new task
+export const createTask = async (token, taskData) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await api.post("/api/tasks", taskData, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Get all tasks
+export const getAllTasks = async (token) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await api.get("/api/tasks", config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Get a single task by ID
+export const getTask = async (token, taskId) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await api.get(`/api/tasks/${taskId}`, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Update a task by ID
+export const updateTask = async (token, taskId, taskData) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await api.put(`/api/tasks/${taskId}`, taskData, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Delete a task by ID
+export const deleteTask = async (token, taskId) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await api.delete(`/api/tasks/${taskId}`, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const taskApi = {
+  createTask,
+  getAllTasks,
+  getTask,
+  updateTask,
+  deleteTask,
+};
+
+export default taskApi;
