236eda7aec49ee5df304adb9c5686543e4ada8ed
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2023-11-30 19:32:04 +0500

    Task Model Updated

--- backend/model/taskSchema.js
+++ backend/model/taskSchema.js
@@ -1,4 +1,43 @@
+// import mongoose from "mongoose";
+
+// const taskSchema = new mongoose.Schema(
+//   {
+//     title: String,
+//     description: String,
+//     estimatedTime: Number,
+//     startDate: Date,
+//     endDate: Date,
+//     comment: String,
+//     taskCategory: String,
+//     assignees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+//     priority: String,
+//     checklist: [
+//       {
+//         text: String,
+//         status: String,
+//       },
+//       { _id: false },
+//     ],
+//     isExpired: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// const Task = mongoose.model("Task", taskSchema);
+// export default Task;
+
 import mongoose from "mongoose";
+
+const checklistItemSchema = new mongoose.Schema({
+  text: String,
+  status: String,
+});
+
+// Set _id to false for the fields in the checklistItemSchema
+checklistItemSchema.set("_id", false);
 
 const taskSchema = new mongoose.Schema(
   {
@@ -7,11 +46,11 @@ const taskSchema = new mongoose.Schema(
     estimatedTime: Number,
     startDate: Date,
     endDate: Date,
-    comments: [String],
+    comment: String,
     taskCategory: String,
-    assignee: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    assignees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     priority: String,
-    checklist: [{ text: String, status: String }],
+    checklist: [checklistItemSchema], // Use the checklistItemSchema here
     isExpired: {
       type: Boolean,
       default: false,
