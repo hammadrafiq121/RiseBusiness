69217ebb75bf161c2bd65229bb31d56d7d7ea867
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2023-12-23 01:55:08 +0500

    Delete Single & Multiple Users

--- frontend/src/components/DeleteUsers.jsx
+++ frontend/src/components/DeleteUsers.jsx
@@ -3,36 +3,41 @@ import { Trash } from "react-bootstrap-icons";
 import Button from "react-bootstrap/Button";
 import DeleteConfirmation from "./DeleteConfirmation";
 import { useDispatch } from "react-redux";
-import { deleteUser } from "../app/reducers/userSlice.js";
+import { deleteUsers } from "../app/reducers/userSlice.js";
 
-const DeleteUser = ({ user }) => {
+const DeleteUsers = ({ selectedUsers }) => {
   const dispatch = useDispatch();
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
   const handleDelete = async () => {
-    await dispatch(deleteUser(user._id));
+    // Perform deletion logic here
+    await dispatch(deleteUsers(selectedUsers));
+
+    // Close the modal
     setShowDeleteModal(false);
   };
 
   return (
     <span>
       <Button
-        variant="link"
-        className="symbol-button"
-        onClick={() => setShowDeleteModal(true)}
+        variant="secondary"
+        type="button"
+        onClick={() => setShowDeleteModal(true)} // Open the modal on button click
+        disabled={selectedUsers.length === 0}
       >
-        <Trash />
+        Delete Users
       </Button>
+
       <DeleteConfirmation
         showModal={showDeleteModal}
         hideModal={() => setShowDeleteModal(false)}
         confirmModal={handleDelete}
-        id={user._id}
-        type="user"
-        message="Are you sure you want to delete this user?"
+        ids={selectedUsers}
+        type="users"
+        message="Are you sure you want to delete these users? if you selected all without filters, it might contain other users in pagination."
       />
     </span>
   );
 };
 
-export default DeleteUser;
+export default DeleteUsers;
