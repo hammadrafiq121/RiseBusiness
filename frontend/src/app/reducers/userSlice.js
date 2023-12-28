5221196f7ac5788533c96a5dd823c5faef3ab06a
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2023-12-29 01:48:05 +0500

    removeUsers Updated

--- frontend/src/app/reducers/userSlice.js
+++ frontend/src/app/reducers/userSlice.js
@@ -69,6 +69,20 @@ export const assignUsers = createAsyncThunk(
   }
 );
 
+export const removeUsers = createAsyncThunk(
+  "users/removeUsers",
+  async ({ _id, users }, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      const response = await userApi.removeUsers(token, _id, users);
+      return response.data;
+    } catch (error) {
+      const message = error?.response?.data?.error;
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const deleteUser = createAsyncThunk(
   "users/deleteUser",
   async (userId, thunkAPI) => {
@@ -209,12 +223,47 @@ const userSlice = createSlice({
         state.isError = false;
         state.isSuccess = true;
         state.message = "Users successfully Assigned.";
+        const updatedUsers = action.payload;
+        // Replace the existing users with the updated data
+        state.users = state.users.map((user) => {
+          const updatedUser = updatedUsers.find(
+            (updated) => updated._id === user._id
+          );
+          return updatedUser || user; // Use updated user if found, otherwise use the original user
+        });
       })
       .addCase(assignUsers.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.isSuccess = false;
         state.message = action.payload || "Error Assigning Users";
+      })
+      .addCase(removeUsers.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.isSuccess = false;
+        state.message = "Removing users...";
+      })
+      .addCase(removeUsers.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.message = "Users successfully Removed.";
+        const updatedUsers = action.payload;
+        // Replace the existing users with the updated data
+        state.users = state.users.map((user) => {
+          const updatedUser = updatedUsers.find(
+            (updated) => updated._id === user._id
+          );
+          return updatedUser || user; // Use updated user if found, otherwise use the original user
+        });
+      })
+      .addCase(removeUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.payload || "Error Removing Users";
       })
       .addCase(deleteUsers.pending, (state) => {
         state.isLoading = true;
